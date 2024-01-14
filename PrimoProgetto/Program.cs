d85b273787173c0c7400bc4ec97bcf6faa6b5557
uomma <marcelloleone975@gmail.com>
2024-01-15 00:02:37 +0100

    Aggiungere i file di progetto.

---
+++ PrimoProgetto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimoProgetto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number of rows");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("number of columns");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("number of columns");
+            int coludns = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
